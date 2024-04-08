6ef05cf5dfd5a9065b118a088060b688f72dfeb0
Thémis Tran Tu Thien <themis.trantuthien@gmail.com>
2024-04-08 22:11:57 +0200

    S'il te plait marche

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct 
+{
+    int x; //player position on the x axis
+    int y; //player position on the y axis
+    int pv; // player health, must be above 0
+    float exp; //player experience point
+    int* bag;  // inventory of the player, contain object
+}player;
+
+
+
+
+
+
+
+int main(){
+    int seed=-1;
+
+    
+    return 0;
+}
