d40d0ce4e79b0117f0a7895e4cac161b2b320880
LouisNardou <166403933+LouisNardou@users.noreply.github.com>
2024-04-08 17:34:16 +0000

    commentaires

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef struct 
+{
+    int x; //player position on the x axis
+    int y; //player position on the y axis
+    int pv; // player health, must be above 0
+    float exp; //player experience point
+    int* bag;  // inventory of the player, contain object
+}player;
 
 
 
@@ -10,8 +18,7 @@
 
 
 int main(){
-
-
+    int seed=-1;
 
     
     return 0;
