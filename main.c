f9e9473c13c7376e2bed7305db6872f9ec470f0f
Tris <166401431+Triiiss@users.noreply.github.com>
2024-04-24 17:56:35 +0200

    Update main.c
    Ajout de l'utilisation de ncurses
    Ajout de l'option quitter (facultatif)

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <fcntl.h>          //Library that allows to print Unicode text
-#include <io.h>
-//#include <curses.h>  //library that allows to move with arrow keys (need to check docstring)
+#include <locale.h>
+#include <ncurses.h>  //library that allows to move with arrow keys (need to check docstring)
 
 typedef struct 
 {
@@ -14,26 +13,138 @@ typedef struct
     int* bag;  // inventory of the player, contain object
 }Player;
 
+void afficher(int curseur){
+    printw("╓─╴  ╓──╖  ╓─╴       ╶╥╴ ╓─╴\n");  //Print Title Screen
+    printw("║    ║  ║  ╙─╖  ║╲╱║  ║  ║  \n");
+    printw("╙─╴  ╙──╜  ╶─╜ ╶╨╴ ╨╴╶╨╴ ╙─╴\n");
+    printw("      ╶╥╴ ╶╥╴\n");
+    printw("       ╙───╢  ╓──╖       ╶╥──╮  ╥─╴  ╶╥─╮\n");
+    printw("           ║  ║  ║  ║╲ ║  ║  │  ╟╴    ╟─╯\n");
+    printw("          ╶╨╴ ╙──╜  ║ ╲║ ╶╨──╯  ╨─╴  ╶╨╴╲\n");
+
+    printw("\n\n");
+
+    printw("  New Game\n");         //Options
+    printw("  Continue\n");
+    printw("  Settings\n");
+    printw("  Quit\n");
+
+    mvprintw(9+curseur,0,"⁕");      //Print the option your hovering on
+    mvprintw(13,0,"");          //Move cursor to the end
+}
+
+int quitter(){
+    int quitter = 0;
+    int k=-1;
+    while (k!=10){
+        clear();
+        printw("Do you really want to quit ?");
+        printw("\n\n");
+        printw("  No        Yes");
+        mvprintw(2,quitter*11,"⁕");
+        mvprintw(3,0,"");
+        refresh();
+        k = getch();
+
+        switch(k){
+            case 260:
+                if (quitter){
+                    quitter = 0;
+                }
+                break;
+            case 261:
+                if (quitter == 0){
+                    quitter = 1;
+                }
+                break;
+        }
+    }
+    if (quitter){
+        k=-1;
+        quitter = 0;
+        while (k!=10){
+            clear();
+            printw("Are you sure you want to give up on this game ?");
+            printw("\n\n");
+            printw("  No        No         Yes");
+            mvprintw(2,quitter*11,"⁕");
+            mvprintw(3,0,"");
+            refresh();
+            k = getch();
+
+            switch(k){
+                case 260:
+                    if (quitter>0){
+                        quitter--;
+                    }
+                    break;
+                case 261:
+                    if (quitter<2){
+                        quitter++;
+                    }
+                    break;
+            }
+        }
+        if (quitter==2){
+            clear();
+            printw("What have you done?");
+            refresh();
+            getch();
+            return 1;
+        }
+
+    }
+    return 0;
+}
+
 int titleScreen(){
-    int test=-1;
-    _setmode(_fileno(stdout),_O_U16TEXT);       //Allows to print Unicode text 
-    wprintf(L"╓─╴  ╓──╖  ╓─╴       ╶╥╴ ╓─╴\n");  //Title
-    wprintf(L"║    ║  ║  ╙─╖  ║╲╱║  ║  ║  \n");
-    wprintf(L"╙─╴  ╙──╜  ╶─╜ ╶╨╴ ╨╴╶╨╴ ╙─╴\n");
-    wprintf(L"      ╶╥╴ ╶╥╴\n");
-    wprintf(L"       ╙───╢  ╓──╖       ╶╥──╮  ╥─╴  ╶╥─╮\n");
-    wprintf(L"           ║  ║  ║  ║╲ ║  ║  │  ╟╴    ╟─╯\n");
-    wprintf(L"          ╶╨╴ ╙──╜  ║ ╲║ ╶╨──╯  ╨─╴  ╶╨╴╲\n");
+    int curseur = 0;
+    int k = -1;
 
-    printf("\n\n");
+    setlocale(LC_ALL,"");           //Set local for unicode characters
+    initscr();
+        keypad(stdscr, TRUE);       //For arrow keys
 
-    wprintf(L"New Game\n");                 //Choices
-    wprintf(L"Continue Game\n");
-    wprintf(L"Quit\n");
-    
-    printf("%d",test);
-    //test=getch();
-    printf("%d",test);
+        afficher(0);
+        k = getch();
+        while (k != 27){            //Until you press escape
+            switch (k){
+                case 258:           //Down
+                    curseur++;
+                    break;
+                case 259:           //Up
+                    curseur--;
+                    break;
+                case 10:
+                    switch(curseur){
+                        case 0:                         //New game
+                            break;
+                        case 1:                         //Load save
+                            break;
+                        case 2:                         //Settings
+                            break;
+                        case 3:
+                            if (quitter()){           //Quit
+                                return 0;
+                            }
+                            break;
+                    }
+            }
+            
+            if (curseur == -1){
+                curseur = 3;
+            }
+            curseur = curseur%4;
+
+            clear();
+            afficher(curseur);
+            refresh();
+
+            k= getch();
+        }
+
+
+    endwin();
 
     return 0;
 }
@@ -42,11 +153,11 @@ int titleScreen(){
 int main(){
     int seed=-1;
     int error = 0;
-
+    
     error = titleScreen();
 
     if (error){            //Print the error if one (if menu return != 0) => Robustesse
-        printf("Ceci est un message d'erreur");
+        printw("Ceci est un message d'erreur");
         return -1;
     }
     
